aefee129636a222dbc33242c531b51e8ab0a79d6
Randal <randal.t.meyer@gmail.com>
2024-09-07 21:10:41 -0700

    comments, imrpove comments regex

--- build/internal/xRegExp.js
+++ build/internal/xRegExp.js
@@ -1,12 +1,12 @@
 import XRegExp from "xregexp";
 const DEBUG = false;
 export function xRegExp(regex, flags) {
-    if (DEBUG) {
+    if (DEBUG || !flags.includes("x")) {
         return XRegExp(regex, flags);
     }
     regex = regex
-        .replace(/\#.+\n/g, "")
+        .replace(/(?<!\\)\#.+\n/g, "")
         .replace(/[\n\t\s]/g, "");
     flags = flags.replace(/x/g, "");
-    return new RegExp(regex, flags);
+    return XRegExp(regex, flags);
 }
